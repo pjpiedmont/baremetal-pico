d35473e3c2eb532d162423088c8f4fff4b9390ba
Parker Piedmont <pjpiedmont@gmail.com>
2025-07-14 21:34:57 -0700

    Zero .bss and copy .data to RAM

--- main.c
+++ main.c
@@ -1,6 +1,31 @@
+int main(void) {
+    return 0;
+}
+
 // Startup code
 __attribute__((naked, noreturn)) void _reset(void) {
-    while (1) (void) 0;
+    // memset .bss to zero and copy .data section to RAM region
+    extern long _sbss;
+    extern long _ebss;
+    extern long _sdata;
+    extern long _edata;
+    extern long _sidata;
+
+    for (long *dst = &_sbss; dst < &_ebss; dst++)
+    {
+        *dst = 0;
+    }
+
+    for (long *dst = &_sdata, *src = &_sidata; dst < &_edata;)
+    {
+        *dst++ = *src++;
+    }
+
+    main();
+
+    while (1) {
+        (void) 0;  // infinite loop if main ever returns
+    }
 }
 
 extern void _estack(void);  // Defined in link.ld
